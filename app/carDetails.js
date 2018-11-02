1798f0e537b86f5b1b93143315d53e7f4e11e32c
jsmuster <jsmastery@gmail.com>
2018-11-02 11:54:00 -0400

    removed misc files

--- app/carDetails.js
+++
@@ -1,9 +0,0 @@
-qq.configure("container", ".container.containerbg");
-
-qq.configure("controllers", "modules/controller/");
-qq.configure("views", "modules/view/");
-
-/* we can register a module with id only, so then controller and view attributes are same as id */
-qq.loadModule({id:"carDetails", controller:"carDetails", view:"carDetails", viewSelector:"#carDetailsPageView"});
-
-qq.init();
